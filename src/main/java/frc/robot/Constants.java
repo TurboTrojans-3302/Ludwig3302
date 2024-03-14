91326fbd3db043b4c4c6defe818c21255ee12b94
Ted Hansen <kc8nod@gmail.com>
2024-03-14 18:49:59 -0400

    first draft of drive heading

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -65,6 +65,9 @@ public final class Constants {
     public static final int kRearRightTurningCanId = 7;
 
     public static final boolean kGyroReversed = false;
+    public static final double headingP = 0;
+    public static final double headingI = 0;
+    public static final double headingD = 0;
   }
 
   public static final class ModuleConstants {
